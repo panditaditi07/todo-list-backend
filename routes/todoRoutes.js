1e0b39ca7e0e49f3b24929d52c7ec2b73df84b41
panditaditi07 <aditi.pandit@raweng.com>
2020-12-09 22:01:48 +0530

    modified taskaControllers and routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,10 +3,14 @@ const {
   getAllTasks,
   verifyPostRequest,
   createTask,
+  verifyPatchRequest,
 } = require("../controllers/taskController");
 const { getbyId, updateTask } = require("../controllers/taskController");
 const router = express.Router();
 
 router.route("/tasks").get(getAllTasks).post(verifyPostRequest, createTask);
-router.route("/tasks/:taskId").get(getbyId).patch(getbyId, updateTask);
+router
+  .route("/tasks/:taskId")
+  .get(getbyId)
+  .patch(getbyId, verifyPatchRequest, updateTask);
 module.exports = router;
